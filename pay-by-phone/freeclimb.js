b4745ad55a509cb1a68475a37d3da156886e993f
Matt Rohan <mrohan@vailsys.com>
2021-04-22 12:29:53 -0500

    change AUTH_TOKEN to API_KEY

--- pay-by-phone/freeclimb.js
+++ pay-by-phone/freeclimb.js
@@ -1,7 +1,7 @@
 require('dotenv-safe').config()
 const freeclimbSDK = require('@freeclimb/sdk')
 const accountId = process.env.ACCOUNT_ID
-const authToken = process.env.AUTH_TOKEN
+const authToken = process.env.API_KEY
 const freeclimb = freeclimbSDK(accountId, authToken)
 
 module.exports = freeclimb
