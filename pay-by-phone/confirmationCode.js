ba475cf135fd962c26939e727bcf83bed33981f0
Matt Rohan <mrohan@vailsys.com>
2021-04-23 09:16:42 -0500

    un-comment implementation for confirmationCode.generate

--- pay-by-phone/confirmationCode.js
+++ pay-by-phone/confirmationCode.js
@@ -1,4 +1,3 @@
 exports.generate= () => {
-   // return Math.floor(100000 + Math.random() * 900000).toString() // create verification code as random 6 digit number represented with a string
-   return 555666
+   return Math.floor(100000 + Math.random() * 900000).toString() // create verification code as random 6 digit number represented with a string
 }
